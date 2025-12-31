ed4511acd3a65a1eb5629f52eeb64a3ce26ab408
Ariyan <ariyanshokrizadeh@gmail.com>
2025-12-31 21:59:46 +0330

    Add plot_result function

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,9 @@ import json
 import re
 from pathlib import Path
 
+import seaborn as sns
+from matplotlib import pyplot as plt
+
 from .config_loader import AppConfig
 from .logger import get_logger
 
@@ -54,3 +57,37 @@ def parse_judgments(file_path: str):
         logger.error(f"Invalid JSON in judgments file '{file_path}': {e}")
         raise
     return data
+
+
+def plot_results(bm25_results, uni_results, bi_results):
+    # 1. Setup Theme
+    sns.set_theme(style="whitegrid", context="paper", font_scale=1.3)
+
+    results = {"BM25": bm25_results, "Unigram": uni_results, "Bigram": bi_results}
+    models = list(results.keys())
+
+    metrics_data = [
+        ("MAP", [r["MAP"] for r in results.values()]),
+        ("P@5", [r["P@5"] for r in results.values()]),
+        ("MRR", [r["MRR"] for r in results.values()]),
+    ]
+
+    fig, axes = plt.subplots(
+        1, 3, figsize=(15, 6), constrained_layout=True, sharey=True
+    )
+    for ax, (metric_name, scores) in zip(axes, metrics_data):
+        sns.barplot(
+            x=models, y=scores, ax=ax, palette="viridis", hue=models, legend=False
+        )
+        ax.set_box_aspect(1)
+        ax.set_title(metric_name, fontweight="bold", pad=10)
+        ax.set_ylabel("Score")
+        ax.set_ylim(0, 0.6)
+
+        for container in ax.containers:
+            ax.bar_label(container, fmt="%.3f", padding=3, fontsize=11)
+
+    fig.suptitle("Retriever Performance Comparison", fontsize=18, fontweight="bold")
+
+    plt.savefig(config.results_plot, dpi=300)
+    print("Plot saved successfully.")
